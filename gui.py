91636f538b33862512e5ed7446366ca3ba548452
jwyjohn <jwyjohn@foxmail.com>
2022-04-23 16:29:17 +0800

    添加指定路径功能

--- gui.py
+++ gui.py
@@ -4,7 +4,8 @@ from tkinter import messagebox
 import time
 from joinmeeting import *
 
-l24 = ["{:0>2d}:{:0>2d}:00".format(h,m) for h in range(24) for m in range(60)]
+l24 = ["{:0>2d}:{:0>2d}:00".format(h, m) for h in range(24) for m in range(60)]
+
 
 class timerTM():
     def __init__(self):
@@ -12,6 +13,7 @@ class timerTM():
         self.cid = ""
         self.pwd = ""
         self.eta = ""
+        self.tmpath = r'C:\Program Files (x86)\Tencent\WeMeet\wemeetapp.exe'
 
         self.root = tk.Tk()
         self.root.title("lazyTM")
@@ -20,20 +22,30 @@ class timerTM():
         self.label_cid = tk.Label(text="Course ID", font=('Fixedsys'))
         self.label_cid.grid(column=0, row=0)
         self.input_cid = tk.Entry(self.root, width=11, font=('Fixedsys'))
+        self.input_cid.insert(0, "9906955801")
         self.input_cid.grid(column=5, row=0)
 
         self.label_pwd = tk.Label(text="Password ", font=('Fixedsys'))
         self.label_pwd.grid(column=0, row=1)
         self.input_pwd = tk.Entry(self.root, width=11, font=('Fixedsys'))
+        self.input_pwd.insert(0, "7737")
         self.input_pwd.grid(column=5, row=1)
 
         self.label_timeset = tk.Label(text="Enter at ", font=('Fixedsys'))
         self.label_timeset.grid(column=0, row=2)
         # self.preset = ttk.Combobox(self.root, values=tuple(
         #     ["07:50:00", "09:50:00", "13:20:00", "15:20:00", "18:50:00"]), width=7, state='readonly')
-        self.preset = ttk.Combobox(self.root, values=tuple(l24), width=7, state='readonly')
+        self.preset = ttk.Combobox(
+            self.root, values=tuple(l24), width=7, state='readonly')
         self.preset.current(0)
         self.preset.grid(column=5, row=2)
+
+        self.label_tmpath = tk.Label(text="TM Path ", font=('Fixedsys'))
+        self.label_tmpath.grid(column=0, row=3)
+        self.input_tmpath = tk.Entry(self.root, width=11, font=('Fixedsys'))
+        self.input_tmpath.insert(
+            0, r'C:\Program Files (x86)\Tencent\WeMeet\wemeetapp.exe')
+        self.input_tmpath.grid(column=5, row=3)
 
         self.commit_btn = tk.Button(
             self.root, text="Commit", font=('Fixedsys'), command=self.commit)
@@ -55,7 +67,7 @@ class timerTM():
         if self.state == "Committed":
             if now[:5] == self.eta[:5]:
                 try:
-                    enter_meeting(self.cid, self.pwd, "mewo~")
+                    enter_meeting(self.cid, self.pwd, "mewo~", self.tmpath, 5)
                 except:
                     messagebox.showerror("Fatal", "Failed entering meeting..")
                 self.root.destroy()
@@ -65,6 +77,7 @@ class timerTM():
         self.input_cid.configure(state='readonly')
         self.input_pwd.configure(state='readonly')
         self.commit_btn.configure(state='disabled')
+        self.input_tmpath.configure(state='readonly')
         self.preset.configure(state='disabled')
 
         try:
@@ -72,14 +85,16 @@ class timerTM():
             self.cid = self.input_cid.get().strip(' ')
             self.pwd = self.input_pwd.get().strip(' ')
             self.eta = self.preset.get().strip(' ')
+            self.tmpath = self.input_tmpath.get()
             self.label_eta.configure(text="[E {}]".format(self.eta))
 
-            messagebox.showinfo("Info", "Timer set at {} enter cid={} with pwd={}".format(
-                self.eta, self.cid, self.pwd))
+            messagebox.showinfo("Info", "Timer set at {} enter cid={} with pwd={}\ntmpath={}".format(
+                self.eta, self.cid, self.pwd, self.tmpath))
+            print(self.tmpath)
         except:
             messagebox.showerror("Fatal", "Unknown Err, quitting..")
             self.root.destroy()
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app = timerTM()
--- joinmeeting.py
+++ joinmeeting.py
@@ -4,36 +4,36 @@ from pywinauto.keyboard import send_keys
 from time import sleep
 from subprocess import Popen
 
-tm_path = r'C:\Program Files (x86)\Tencent\WeMeet\wemeetapp.exe'
+# tm_path = r'C:\Program Files (x86)\Tencent\WeMeet\wemeetapp.exe'
 
-def enter_meeting(cid, pwd, uname):
+def enter_meeting(cid, pwd, uname, tm_path, timeout):
     Popen(tm_path, shell=True)
     dlg = Desktop(backend="uia").window(title="腾讯会议(LoadingWnd)")
     dlg.wait('visible ready')
-    sleep(1)
+    sleep(timeout)
     # dlg.print_control_identifiers()
     dlg['加入会议'].click_input()
-    sleep(1)
+    sleep(timeout)
     dlg = Desktop(backend="uia").window(title="腾讯会议(JoinWnd)")
-    sleep(1)
+    sleep(timeout)
     if dlg['入会开启麦克风'].get_toggle_state():
         dlg['入会开启麦克风'].click_input()
-    sleep(1)
+    sleep(timeout)
     if dlg['入会开启摄像头'].get_toggle_state():
         dlg['入会开启摄像头'].click_input()
-    sleep(1)
+    sleep(timeout)
     dlg['会议号Edit'].click_input()
-    sleep(1)
+    sleep(timeout)
     send_keys("c^a{BACKSPACE}")
-    sleep(1)
+    sleep(timeout)
     send_keys(str(cid))
-    sleep(1)
+    sleep(timeout)
     dlg['Join_meeting_Join_meeting'].click_input()
-    sleep(1)
+    sleep(timeout)
     dlg = Desktop(backend="uia").window(title="")
-    sleep(1)
+    sleep(timeout)
     send_keys(str(pwd)+"{ENTER}")
-    sleep(1)
+    sleep(timeout)
     dlg = Desktop(backend="uia").window(title="腾讯会议(InMeetingWnd)")
     print("[Done] joined meeting {} with name = {} .".format(cid,uname))
 
